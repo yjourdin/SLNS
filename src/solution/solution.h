51edeae47cb6d105344a31ef34b4c6800ee07b08
Yann Jourdin <yjourdin2001@gmail.com>
2023-06-21 17:23:43 +0200

    Minor rewriting Solution

--- src/solution/solution.h
+++ src/solution/solution.h
@@ -2,17 +2,23 @@
 
 #include <memory>
 
-#include "../cost.h"
+#include "../cost/cost.h"
 #include "../instance/instance.h"
 #include "../types.h"
 
 class Solution {
  public:
   virtual bool is_feasible() const = 0;
-  virtual Cost get_cost() const = 0;
   virtual bool is_better(Solution& other) const = 0;
+  virtual std::shared_ptr<Solution> copy() const = 0;
+  virtual void compute_cost() = 0;
+  virtual const unsigned get_size() const = 0;
+  const std::shared_ptr<Instance> get_instance() const { return instance; };
+  std::shared_ptr<Cost> get_cost() const {
+    return std::make_shared<Cost>(cost);
+  };
 
- private:
-  std::shared_ptr<Instance> instance;
-  Cost cost;
+ protected:
+  const std::shared_ptr<Instance> instance;
+  std::unique_ptr<Cost> cost;
 };
