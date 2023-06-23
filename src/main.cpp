bd9a37348b9ff63a873d6c71e533c3892186fb1d
Yann Jourdin <yjourdin2001@gmail.com>
2023-06-23 18:18:59 +0200

    uppercase to own types v2

--- src/main.cpp
+++ src/main.cpp
@@ -30,15 +30,11 @@ int main(int argc, char* argv[]) {
 
   // SLNS parameters
 
-  destruction_size small_destruction_size_min = 1;
-  destruction_size small_destruction_size_max = 20;
-  destruction_size large_destruction_size_min = 20;
-  destruction_size large_destruction_size_max = 80;
-  std::vector<DestroyOperator> small_destroy_operators = {RandomRemoval()};
-  std::vector<DestroyOperator> large_destroy_operators = {RandomRemoval()};
-  std::vector<RepairOperator> small_repair_operators = {Random()};
-  std::vector<RepairOperator> large_repair_operators = {Random()};
-  iteration_count LNS_freq = 10 * std::pow(instance.get_nb_clients(), 1.5);
+  Destruction_size small_destruction_size_min = 1;
+  Destruction_size small_destruction_size_max = 20;
+  Destruction_size large_destruction_size_min = 20;
+  Destruction_size large_destruction_size_max = 80;
+
   auto stop = std::make_unique<StopCriterion>(StopCriterionSet());
   stop.insert("iteration", std::make_unique(IterationLimit(25000)));
   stop.insert("iter_without_improvement",
@@ -58,10 +54,9 @@ int main(int argc, char* argv[]) {
 
   slns.initialise(best);
 
-  route_count nb_route = best->get_nb_routes();
-  do
-  {
-    RouteRemoval route_removal();
+  Route_count nb_route = best->get_nb_routes();
+  do {
+    RouteRemoval route_removal;
     route_removal(best, 1);
     best->remove_empty_routes();
     best = slns.run(best, random_seed);
