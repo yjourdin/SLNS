86c8521320a76ea0ea2d5347ab18f4d72f07a945
Yann Jourdin <yjourdin2001@gmail.com>
2023-06-26 16:22:53 +0200

    Add Coordinate_type to discrete compile parameters

--- src/types.h
+++ src/types.h
@@ -10,16 +10,17 @@ using Iteration_count = unsigned long;
 // VRP
 
 #ifdef DISCRETE
+using Coordinate_type = int;
 using Distance = unsigned;
 using Duration = unsigned;
 using Time = unsigned;
 #else
+using Coordinate_type = double;
 using Distance = double;
 using Duration = double;
 using Time = double;
 #endif
 using Capacity = unsigned short;
-using Coordinate_type = double;
 using Node_index = unsigned;
 using Node_count = unsigned;
 using Route_count = unsigned short;
